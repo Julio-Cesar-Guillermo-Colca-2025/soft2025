f948b8ecae83f984ae32e25c81aa9f88a01dff33
Julio Cesar <tu-email@ejemplo.com>
2025-05-12 21:27:02 -0500

    Limpieza total para reinicio de GitHub Pages

--- grupo-a/src/utils/highlight.ts
+++
@@ -1,8 +0,0 @@
-import * as shiki from 'shiki';
-
-const highlighterPromise = shiki.createHighlighter({ themes: ['nord'] });
-
-export async function highlightCode(code: string, lang: string = 'javascript') {
-  const highlighter = await highlighterPromise;
-  return highlighter.codeToHtml(code, { lang, themes: ['nord'] });
-}
